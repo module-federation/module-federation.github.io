a1b4a5dcdd98e893970844dba8cc8c1b682e58d4
zackjackson <zackary.l.jackson@gmail.com>
2020-05-16 21:42:06 -0700

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,22 @@ import 'semantic-ui-css/semantic.min.css';
 
 import config from '../data/config.json'
 import '../styles/bootstrap.css';
+const tracking = `<!-- Global site tag (gtag.js) - Google Analytics -->
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-120967034-1');
+`
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>{config.title}</title>
-        <meta name="description" content={config.description}></meta>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120967034-1"></script>
 
+        <meta name="description" content={config.description}></meta>
+        <script dangerouslySetInnerHTML={{__html:tracking}}/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
