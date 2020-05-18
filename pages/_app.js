5e0133fda2be5f55c7a65eb8e438da3dd6cfcefb
Zack Jackson <zackary.l.jackson@gmail.com>
2020-05-17 17:25:54 -0700

    fix: adding analytics data (#10)
    * fix: adding analytics data
    
    * fix: adding analytics data

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,23 @@ import 'semantic-ui-css/semantic.min.css';
 
 import config from '../data/config.json'
 import '../styles/bootstrap.css';
-const tracking = `<!-- Global site tag (gtag.js) - Google Analytics -->
+const tracking = `
+function getUrlParams(search) {
+    const hashes = search.slice(search.indexOf('?') + 1).split('&')
+    const params = {}
+    hashes.map(hash => {
+        const [key, val] = hash.split('=')
+        params[key] = decodeURIComponent(val)
+    })
+    return params
+}
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-120967034-1');
+  var redirect = getUrlParams(window.location.href).medium
+  window.location.replace = redirect
 `
 function MyApp({ Component, pageProps }) {
   return (
@@ -31,3 +42,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+//http://localhost:3000/?utm_source=ModuleFederationExamples&utm_medium=https://github.com/sponsors/ScriptedAlchemy&utm_campaign=sponsor_link
